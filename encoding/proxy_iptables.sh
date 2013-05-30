fe9554d886dbec88deeafa0f338d83a0bdb69845
Gregoire Delannoy <gregoire.delannoy@free.fr>
2013-05-30 11:43:29 +0100

    File structure re-organization

--- encoding/proxy_iptables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-ifconfig tun0 192.168.13.1 pointopoint 192.168.13.2
-iptables -t nat -F
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-echo 1 > /proc/sys/net/ipv4/ip_forward
--- network/client_iptables.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-ifconfig tun0 192.168.13.2 pointopoint 192.168.13.1
-
-ip route add default via 192.168.13.1
